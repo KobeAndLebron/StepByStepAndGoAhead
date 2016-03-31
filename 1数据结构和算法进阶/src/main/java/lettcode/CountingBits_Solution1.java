74e2c9264629960949c7ed61cc616405075d8af0
jingshuai.chen <1005871594@qq.com>
2016-03-31 20:09:12 +0800

    Counting_bits compelete-1

--- 1数据结构和算法进阶/src/main/java/lettcode/CountingBits_Solution1.java
+++ 1数据结构和算法进阶/src/main/java/lettcode/CountingBits_Solution1.java
@@ -1,7 +1,11 @@
 package lettcode;
 
 /**
+ * Counting Bits:
+ * 	Given a non negative integer number num.For every numbers i in the range 0=<i<=num 
+ * calculate the number of 1's in their binary representation and return them as an array
  * 
+ * 暴力递归
  * @author ChenJingShuai
  *
  * 每天进步一点-2016年3月31日-上午9:02:58
@@ -18,9 +22,9 @@ public class CountingBits_Solution1 {
 					break;
 				}
 			}
-			if (Math.pow(2, n) == num) {
+			if (Math.pow(2, n) == num) { // 如果正好是最高位是1
 				return 1;
-			} else {
+			} else { //  减去最高位为1代表的数，然后算出剩下的数的二进制数即可---递归关键
 				return getBits((int) (num - Math.pow(2, n - 1))) + 1;
 			}
 		} else if (num == 0) {
@@ -39,7 +43,9 @@ public class CountingBits_Solution1 {
 	}
 
 	public static void main(String[] args) {
-		int[] nums = countBits(5);
-		System.out.println(nums);
+		long s = System.currentTimeMillis();
+		int[] nums = countBits(10000);
+		long e = System.currentTimeMillis();
+		System.out.println(nums + " , time : " + (e - s));
 	}
 }
--- 1数据结构和算法进阶/src/main/java/lettcode/CountingBits_Solution2.java
+++ 1数据结构和算法进阶/src/main/java/lettcode/CountingBits_Solution2.java
@@ -5,6 +5,7 @@ package lettcode;
  * 	Given a non negative integer number num.For every numbers i in the range 0=<i<=num 
  * calculate the number of 1's in their binary representation and return them as an array
  * 
+ * 动态规划
  * 
  * @author ChenJingShuai
  *
@@ -13,8 +14,11 @@ package lettcode;
 public class CountingBits_Solution2 {
 	public static int getBits(int num ,int[] array) {
 		int numOf1 = 0;
+		if(array[num] != 0){
+			return array[num];
+		}
 		// 如果array[num]里面的值不为0，那么就证明此值已经被求过了
-		if (num > 2 && array[num] != 0) {
+		if (num > 2) {
 			int n = 0;
 			/** 
 			 * 算出最大的小于num的2的n次方的数
@@ -26,17 +30,16 @@ public class CountingBits_Solution2 {
 			}
 			if (Math.pow(2, n) == num) {
 				numOf1 = 1;
-				array[num] = numOf1;
 			} else {
 				numOf1 = getBits((int) (num - Math.pow(2, n - 1)) ,array) + 1;
-				array[num] = numOf1;
 			}
 		} else if (num == 0) {
 			numOf1 = 0;
 		} else {
 			numOf1 = 1;
 		}
-		return array[num];
+		array[num] = numOf1;
+		return numOf1;
 	}
 
 	public static int[] countBits(int num) {
@@ -49,7 +52,9 @@ public class CountingBits_Solution2 {
 	}
 
 	public static void main(String[] args) {
-		int[] nums = countBits(5);
-		System.out.println(nums);
+		long s = System.currentTimeMillis();
+		int[] nums = countBits(10000);
+		long e = System.currentTimeMillis();
+		System.out.println(nums + " , time : " + (e - s));
 	}
 }
