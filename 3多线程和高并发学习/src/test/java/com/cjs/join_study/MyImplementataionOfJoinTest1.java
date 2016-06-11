78a3ec2a54b95edd2c10ad0d18f5b0cd2fcfbdae
ChenJingShuai <1005871594@qq.com>
2016-06-11 18:37:54 +0800

    commit commit

--- 3多线程和高并发学习/src/test/java/com/cjs/join_study/MyImplementataionOfJoinTest1.java
+++ 3多线程和高并发学习/src/test/java/com/cjs/join_study/MyImplementataionOfJoinTest1.java
@@ -1,12 +1,12 @@
 package com.cjs.join_study;
 
+import org.junit.Test;
+
 import com.cjs.TimerThread;
 
 /**
- * 自己实现的join方法-测试1:
- * 	当线程sleep（并且拿着自己的锁）的时间大于join的时间的时候，join的时间就会失效；因为join和run方法公用的是一把锁:线程对象
- * 
- * 	让join的timeout失效的方法
+ * 自己实现的join方法-测试1-让join的timeout失效的方法:
+ * 	当线程sleep（并且拿着自己的锁）的时间大于join的时间的时候，join的时间就会失效；因为join和run方法公用的是一把锁:此线程对象
  * 
  * @see ImplementationPrincipleOfJoin
  * @author ChenJingShuai
@@ -14,7 +14,9 @@ import com.cjs.TimerThread;
  * 每天进步一点-2016年4月12日-下午9:17:17
  */
 public class MyImplementataionOfJoinTest1 {
+	// 用于保证先执行join方法
 	private static final int mills = 1000;
+	// 线程sleep的时间
 	private static final int mills1 = 10000;
 	private static final Thread t = new Thread(new Runnable() {
 		public void run() {
@@ -34,7 +36,7 @@ public class MyImplementataionOfJoinTest1 {
 		}
 	});
 	
-	public static void join(Thread thread , long timemills) throws InterruptedException{
+	public static void join(Thread thread, long timemills) throws InterruptedException{
 		synchronized (thread) {
 			// 现在的时间
 			long now = System.currentTimeMillis();
@@ -42,7 +44,7 @@ public class MyImplementataionOfJoinTest1 {
 			long waitedTime = 0;
 			
 			if(timemills < 0){
-				throw new IllegalArgumentException("time out is negative");
+				throw new IllegalArgumentException("Time out is negative");
 			}
 			
 			if(timemills == 0){
@@ -51,6 +53,7 @@ public class MyImplementataionOfJoinTest1 {
 				}
 			}else{
 				while(thread.isAlive()){
+					// 所剩的时间
 					long delay = timemills - waitedTime;
 					if(delay <= 0){
 						break;
@@ -62,12 +65,13 @@ public class MyImplementataionOfJoinTest1 {
 		}
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
+	@Test
+	public void test() throws InterruptedException {
 		TimerThread th = new TimerThread(mills + mills1);
 		th.start();
 		t.start();
+		// 3000ms expires
 		MyImplementataionOfJoinTest1.join(t, 3000);
-		System.out.println("I wanted CPU!!! but bj Thread is " + (t.isAlive() ? "alive" : "dead"));
 	}
 
 }
