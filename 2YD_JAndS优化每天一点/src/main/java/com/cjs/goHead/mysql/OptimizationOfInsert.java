fb2e20ad00bcb73647074a6211f5862ae51e8dfb
ChenJingShuai <1005871594@qq.com>
2016-06-28 20:05:36 +0800

    syntax error modified

--- 2YD_JAndS优化每天一点/src/main/java/com/cjs/goHead/mysql/OptimizationOfInsert.java
+++ 2YD_JAndS优化每天一点/src/main/java/com/cjs/goHead/mysql/OptimizationOfInsert.java
@@ -3,11 +3,11 @@ package com.cjs.goHead.mysql;
 /**
  * 
  * 1 一条SQL语句插入多条数据。
- * 	Insert into table 'insert_table' values (...);
- *  Insert into table 'insert_table' values (...);
- * 	Insert into table 'insert_table' values (...); ...
+ * 	Insert into 'insert_table' values (...);
+ *  Insert into 'insert_table' values (...);
+ * 	Insert into 'insert_table' values (...); ...
  * 	-------------->
- * 	Insert into table 'insert_table' values (...),(...),(...),...;
+ * 	Insert into 'insert_table' values (...),(...),(...),...;
  * 	
  * 		通过合并SQL语句，减少了SQL语句解析的次数，减少了网络传输的I/O
  * 		减少了日志-binlog日志和innodb的事务让日志量，降低了日至刷盘的数据量和频率，从而提高了效率。
