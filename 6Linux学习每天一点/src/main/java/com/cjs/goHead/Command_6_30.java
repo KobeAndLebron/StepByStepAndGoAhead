2727a37becd9de506fcc1badc6f1ea706877a565
ChenJingShuai <1005871594@qq.com>
2016-06-30 18:36:03 +0800

    add TODOList and whereis

---
+++ 6Linux学习每天一点/src/main/java/com/cjs/goHead/Command_6_30.java
@@ -0,0 +1,19 @@
+package com.cjs.goHead;
+
+
+/**
+ * 	whereis 
+ * 	
+ *  
+ * 	Usage: whereis [-bmsu] 
+ * 		
+ * 
+ * @author ChenJingShuai  
+ *
+ * Make a bit of progress every day. 2016年6月30日-下午5:40:56
+ *
+ */
+public class Command_6_30 {
+
+}
+ 
---
+++ 6Linux学习每天一点/src/main/java/com/cjs/goHead/TODOList.java
@@ -0,0 +1,18 @@
+package com.cjs.goHead;
+
+
+/**
+ * 
+ * TODO  
+ * 	ifconfig
+ * 	apt-get install/remove/autoremove/autoclean
+ * 	whereis 
+ * 	top
+ * @author ChenJingShuai  
+ *
+ * Make a bit of progress every day. 2016年6月30日-下午5:45:45
+ *
+ */
+public class TODOList {
+
+}
