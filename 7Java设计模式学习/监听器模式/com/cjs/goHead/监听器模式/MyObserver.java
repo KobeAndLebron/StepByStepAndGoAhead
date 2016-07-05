55373a033438a4b87ea9e9fe7a81433415fd72dd
jingshuai.chen <1005871594@qq.com>
2016-07-05 23:05:59 +0800

    add observer design pattern

---
+++ 7Java设计模式学习/监听器模式/com/cjs/goHead/监听器模式/MyObserver.java
@@ -0,0 +1,25 @@
+package com.cjs.goHead.监听器模式;
+
+public class MyObserver implements Observer{
+	private String name;
+	private Subject topic;
+	
+	public MyObserver(String nm){
+		this.name=nm;
+	}
+	@Override
+	public void update() {
+		// get the message of subject update
+		String msg = (String) topic.getUpdate(this);
+		if(msg == null){
+			System.out.println(name+":: No new message");
+		}else
+		System.out.println(name+":: Consuming message::"+msg);
+	}
+
+	@Override
+	public void setSubject(Subject sub) {
+		this.topic=sub;
+	}
+
+}
---
+++ 7Java设计模式学习/监听器模式/com/cjs/goHead/监听器模式/UseOfListener.java
@@ -0,0 +1,13 @@
+package com.cjs.goHead.监听器模式;
+
+/**
+ * 	Observer design pattern is useful when you are interested 
+ * in the state of an object and want to get notified whenever 
+ * there is any change.
+ * @author ChenJingShuai
+ *
+ * 每天进步一点-2016年7月5日-下午10:45:22
+ */
+public class UseOfListener {
+
+}
