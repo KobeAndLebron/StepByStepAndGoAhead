8b4c5664253641c40a830720c68fc05166f3cf43
ChenJingShuai <1005871594@qq.com>
2016-07-06 11:15:55 +0800

    modify

--- merge_master_with_all_other_local_branches_and_push.sh
+++ merge_master_with_all_other_local_branches_and_push.sh
@@ -2,14 +2,14 @@
 current_branch_name=`git branch -l | grep '^* [0-9a-zA-Z]*$' | grep -o '[0-9a-zA-Z]*$'`
 #the branch which will be pushed
 pushed_branch="master"
-echo "The current branch is $current_branch_name"
+echo "The current branch is $current_branch_name\n"
 git checkout $pushed_branch
-echo "1 Switch $pushed_branch"
+echo "1 Switch $pushed_branch\n"
 git merge 450 1>/dev/null
 git merge 480 1>/dev/null
 git merge dell 1>/dev/null
-echo "2 Merge finish"
+echo "2 Merge finish\n"
 git push 1>/dev/null
-echo "3 $pushed_branch is pushed to remote respority"
+echo "3 $pushed_branch is pushed to remote respority\n"
 git checkout $current_branch_name 1>/dev/null
-echo "4 switch $current_branch_name" 
+echo "4 switch $current_branch_name\n" 
