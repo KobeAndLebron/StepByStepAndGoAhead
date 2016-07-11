e66d367e28ef05b457d0eb9f1bbe4c2f407fe895
jingshuai.chen <1005871594@qq.com>
2016-07-11 22:57:04 +0800

    add today log-7-11

---
+++ 6Linux学习每天一点/DayLog/com/cjs/goHead/C2016_07_11.java
@@ -0,0 +1,17 @@
+package com.cjs.goHead;
+
+/**
+ * 	1 做IT(Integrated Test)的时候,要把主代码检查无误之后在进行IT,因为IT比较耗时间,  测完不对后又要改主代码又要跑IT  ,如果检查无误后在进行IT,
+ * 这样节省了跑第二次IT的时间和第二次改主代码然后检查主代码的时间(检查的时间肯定大大的少于这两个时间之和,并且也能增加自己的精神和尽头),可以大大的提高效率
+ * 
+ *  2 NullPointerException Throwing {@code null} also can cause NullPointerException 
+ * as if it were a {@code Throwable} value.
+ *  
+ * 
+ * @author ChenJingShuai
+ *
+ * 每天进步一点-2016年7月11日-下午10:17:25
+ */
+public class C2016_07_11 {
+
+}
