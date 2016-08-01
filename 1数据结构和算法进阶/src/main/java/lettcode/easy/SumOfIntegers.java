04306421d0e2e1932c93180717be77b3c7af8804
ChenJingShuai <1005871594@qq.com>
2016-08-01 21:12:59 +0800

    unfinished

---
+++ 1数据结构和算法进阶/src/main/java/lettcode/easy/SumOfIntegers.java
@@ -0,0 +1,28 @@
+package lettcode.easy;
+
+/**
+ * Calculate the sum of two integers, but you are not allowed to use operator + and -.
+ * 
+ * @author ChenJingShuai 1 Aug 2016
+ *
+ */
+public class SumOfIntegers {
+	public int getSum(int a, int b){
+		int returnI = 0;
+		// binary array of a
+		int[] bitA = new int[1];
+		// binary array of b
+		int[] bitB = new int[2];
+		
+		// final array of binary
+		int[] bitC = new int[3];
+		int i = 0;
+		int j = 0;
+		// add binary array
+		for(;i < bitA.length && j < bitB.length;i++){
+			
+		}
+		
+		return returnI;
+	}
+}
