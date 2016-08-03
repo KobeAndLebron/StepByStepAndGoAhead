ef9fde2839f0ec3b455e765a28efbfc2786e43f7
jingshuai.chen <1005871594@qq.com>
2016-08-03 23:03:46 +0800

    add Servlet dependency for study

--- 7Java设计模式学习/监听器模式/com/cjs/goHead/observer/basic_example/inter/ServletContext.java
+++ 7Java设计模式学习/监听器模式/com/cjs/goHead/observer/basic_example/inter/ServletContext.java
@@ -1,5 +1,12 @@
 package com.cjs.goHead.observer.basic_example.inter;
 
+/**
+ * @see {@linkplain javax.servlet.ServletContext}
+ * 
+ * @author ChenJingShuai
+ *
+ * 每天进步一点-2016年8月3日-下午10:50:10
+ */
 public interface ServletContext {
 	public <T extends ServletContextListener> void addListener(T t);
 	public String getName();
