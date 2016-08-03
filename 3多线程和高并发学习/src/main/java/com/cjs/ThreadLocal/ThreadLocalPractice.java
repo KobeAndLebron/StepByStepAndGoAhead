cf644f4b3351fcd1575b2cf231c8be55e009290b
jingshuai.chen <1005871594@qq.com>
2016-08-03 23:04:53 +0800

    modify tinily

--- 3多线程和高并发学习/src/main/java/com/cjs/ThreadLocal/ThreadLocalPractice.java
+++ 3多线程和高并发学习/src/main/java/com/cjs/ThreadLocal/ThreadLocalPractice.java
@@ -9,26 +9,27 @@ package com.cjs.ThreadLocal;
  * 原理:
  * ThreadLocal里面get方法：
  * 	1、先获取当前线程-Thread.currentThread();
- * 	2、获取当前线程的ThreadLocal.ThreadLocalMap，这个变量完全由ThreadLocal创建和管理，但是引用交给Thread管理。
+ * 	2、获取当前线程的ThreadLocal.ThreadLocalMap，这个变量完全由ThreadLocal创建和操作，但是引用交给Thread管理。
  * 		如果map为空(则创建map)或者此ThreadLocal对象对应的Entry对象为null，就调用initialValue方法（这个方法使用的是模版方法模式，
- * 可以由子类重写改变其行为，默认返回null），然后把这个初始值放入到map的entry数组中;
+ * 可以由子类重写改变其行为，默认返回null），然后把这个初始值放入到ThreadLocalMap的Entry数组中;
  * 	ThreadLocal values pertaining to this thread. This map is maintained
  * 		by the ThreadLocal class. 
  *  ThreadLocal.ThreadLocalMap threadLocals = null;
  *  3、根据当前的线程局部变量(ThreadLocal的threadLocalHashCode)和Entry数组长度(index = key.threadLocalHashCode & 
- *  	(table.length - 1))来获取线程局部变量所对应的值
+ *  	(table.length - 1))来获取此线程局部变量在当前线程的ThreadLocal.ThreadLocalGroup.Entry[]数组的索引
  *  
  *  set相同
  *  特殊的是第二步-不会调用initialValue方法，如果map为空的话，则根据当前线程和set的值还有当前ThreadLocal来设置/创建map
  * 
  * 	Struts2的ActionContext就是用的这种方法，此类的Integer相当于ActionContext，
- *  用这种方法就可以达到每个线程都有自己独立的ActionContext
+ * 用这种方法就可以达到每个线程都有自己独立的ActionContext
+ *  
  * @author 陈景帅
  *
  */
 public class ThreadLocalPractice {
-	
 	final static int initialInteger = 1;
+
 	private static final ThreadLocal<Integer> integerThreadLocal = new ThreadLocal<Integer>(){
 		@Override
 		protected Integer initialValue() {
@@ -59,6 +60,9 @@ public class ThreadLocalPractice {
 class Thread1 extends Thread{
 	public void run(){
 		ThreadLocalPractice.setContext(new Integer(5));
+		/**
+		 * 去掉上面这句话,调用get方法就会调用initialValue方法
+		 */
 		System.out.println(ThreadLocalPractice.getContext());
 	}
 }
