b20696449defc58ad048a4232a4e0bfaead1e76d
jingshuai.chen <1005871594@qq.com>
2016-08-03 23:02:41 +0800

    add diary of 8.3

---
+++ 6Linux学习每天一点/DayLog/com/cjs/goHead/C2016_08_03.java
@@ -0,0 +1,25 @@
+package com.cjs.goHead;
+
+/**
+ * 
+ * The things done during the day:
+ * 	1 巩固ThreadLocal和拦截器的使用
+ *  2 HttpCode 200 ok 500 server internal error 400 url语法错误
+ *  3 监听器模式-Observer pattern 的学习 
+ * 
+ * -----------------------------------------------------------
+ * 
+ *  HAVE DONE ThreadLocal的基本原理和基本使用已经回顾
+ *  		  
+ * 	TODO 
+ * 		1   拦截器的实现.
+ * 		2 ThreadLocal.ThreadLocalMap的实现.
+ *      3 Observer pattern基本例子的创建和学习,使用的例子是Servlet,顺便可以学习一下Servlet-API.
+ *      
+ * @author ChenJingShuai
+ *
+ * 每天进步一点-2016年8月3日-下午10:11:22
+ */
+public class C2016_08_03 {
+	
+}
