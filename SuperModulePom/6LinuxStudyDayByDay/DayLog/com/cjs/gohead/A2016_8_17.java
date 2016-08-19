d1ddbedfc447db6145c718a5d72397592da6bb1e
ChenJingShuai <1005871594@qq.com>
2016-08-19 19:24:02 +0800

    common commit

---
+++ SuperModulePom/6LinuxStudyDayByDay/DayLog/com/cjs/gohead/A2016_8_17.java
@@ -0,0 +1,50 @@
+package com.cjs.gohead;
+
+/**
+ * Mysql 聚合函数count(columnnName)会去除值为null的情况
+ * 		 A1 or A2 and A3 只要A1正确,此结果就为true; (A1 or A2) and A3 A1或者A2正确,并且A3正确,此结果才为true.
+ * 		 Mysql: cast()函数 
+ * @author ChenJingShuai  
+ *
+ * Make a bit of progress every day. 2016年8月17日-下午1:43:13
+ *
+ */
+public class A2016_8_17 {
+
+}
+
+
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+  
+ 
+ 
+ 
+
+
+
+ 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
