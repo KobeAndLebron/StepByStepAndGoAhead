1a1440d1050e12a4b7b3add0ec435bf4c92efe32
jingshuai.chen <1005871594@qq.com>
2016-10-19 18:22:58 +0800

    Commit firstly.

--- SuperModulePom/1DataStructureAndAlgorithmStudy/src/main/java/lettcode/medium/SpiralMatrix.java
+++ SuperModulePom/1DataStructureAndAlgorithmStudy/src/main/java/lettcode/medium/SpiralMatrix.java
@@ -1,6 +1,6 @@
 package lettcode.medium;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -24,8 +24,11 @@ import java.util.List;
  */
 public class SpiralMatrix {
 	public List<Integer> spiralOrder(int[][] matrix) {
-		if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+		if (matrix == null) {
 			return null;
+		}
+		if (matrix.length == 0 || matrix[0].length == 0) {
+			return Collections.EMPTY_LIST;
 		}
 		return getList(matrix, 0, matrix.length - 1, 0, matrix[0].length - 1);
     }
@@ -36,46 +39,39 @@ public class SpiralMatrix {
 	 * @return 螺旋状数组
 	 */
 	private List<Integer> getList(int[][] matrix, int up, int down, int left, int right){
-		List<Integer> outPutList = new ArrayList<>();
+		List<Integer> outPutList = new LinkedList<>();
 
-		if (down > up || right >= left) {
-			List<Integer> upList = new LinkedList<>();
-			List<Integer> downList = new LinkedList<>();
-			if (left <= right) { // return left and right list.
-				for(int i = left; i <= right; i++) {
-					upList.add(matrix[up][i]);
-				}
-				for(int i = right; i <= left; i--) {
-					downList.add(matrix[down][i]);
-				}
+		if (down >= up && right >= left) {
+			// traverse up side.
+			for(int i = left; i <= right; i++) {
+				outPutList.add(matrix[up][i]);
 			}
+			up++;
+			// traverse right side.
+			for(int i = up; i <= down; i++) {
+				outPutList.add(matrix[i][right]);
+			}
+			right--;
 
-			int temp = up + 1;
-			List<Integer> leftList = new LinkedList<>();
-			List<Integer> rightList = new LinkedList<>();
-			if (up < down) { // return up and down list.
+			if (up <= down) {
+				// travel down side.
+				for(int i = right; i >= left; i--) {
+					outPutList.add(matrix[down][i]);
+				}
 				down--;
-				for(int i = up; i <= down; i++) {
-					leftList.add(matrix[i][right]);
+				// travel left side.
+				if (right >= left) {
+					for(int i = down; i >= up; i--) {
+						outPutList.add(matrix[i][left]);
+					}
+					left++;
 				}
-				for(int i = down; i <= up; i--) {
-					rightList.add(matrix[i][left]);
-				}
-
 			}
-
-			outPutList = getList(matrix, up + 1, down - 1, left + 1, right - 1);
-			outPutList.addAll(upList);
-			outPutList.addAll(rightList);
-			outPutList.addAll(downList);
-			outPutList.addAll(leftList);
+		}
+		if (down >= up && right >= left) {
+			outPutList.addAll(getList(matrix, up, down, left, right));
 		}
 
-		// The List which contains up right down is a spiral.
 		return outPutList;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println(new SpiralMatrix().spiralOrder(new int[][]{new int[]{2,3}}));
 	}
 }
