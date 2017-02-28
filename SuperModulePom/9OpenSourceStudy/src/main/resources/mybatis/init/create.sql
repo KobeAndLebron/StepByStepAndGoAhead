b7018f69a1a431392327ceba5bb77e7d4f65ab91
jingshuai.chen <1005871594@qq.com>
2017-02-28 22:52:02 +0800

    initial commit for mybatis study

---
+++ SuperModulePom/9OpenSourceStudy/src/main/resources/mybatis/init/create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE start.student(
+    ID int(10) NOT NULL AUTO_INCREMENT,
+    NAME varchar(100) NOT NULL,
+    SEX varchar(255) NOT NULL,
+    AGE int(3) NOT NULL,
+    PRIMARY KEY (`ID`));
