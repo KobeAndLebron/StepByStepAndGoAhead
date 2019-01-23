aec8ca6e63d72c1d6ad4fcd9670d008dbc1d2e97
jingshuai.chen <1005871594@qq.com>
2019-01-23 11:10:04 +0800

    revise comment

--- SuperModulePom/6Linux/src/main/java/com/cjs/gohead/shell/basic_grammar/variable/specialVariables.sh
+++ SuperModulePom/6Linux/src/main/java/com/cjs/gohead/shell/basic_grammar/variable/specialVariables.sh
@@ -10,7 +10,7 @@ echo $@
 echo $#
 
 # Expands to the name of the shell or shell script
-# (在哪个目录执行脚本, 就显示此脚本在哪个目录的相对路径, 即命令行输入的此脚本的名字).
+# (在哪个目录执行脚本, 就显示此脚本在哪个目录的相对路径, 即命令行输入的此脚本的名字, 例子: 输入$1/test.sh 则$0会代表"$1/test.sh").
 echo $0 # 'readlink -e $0'可以得到文件的全路径.
 
 # loop predefine variables.
