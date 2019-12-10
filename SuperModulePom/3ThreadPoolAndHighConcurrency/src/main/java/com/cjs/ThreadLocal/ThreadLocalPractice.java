41d91cdab471b45febd248b7bc006443080aea40
chenjingshuai <chenjingshuai@k2data.com.cn>
2019-12-10 18:33:30 +0800

    threadLocal原理+WeakReference作用

--- SuperModulePom/3ThreadPoolAndHighConcurrency/src/main/java/com/cjs/ThreadLocal/ThreadLocalPractice.java
+++ SuperModulePom/3ThreadPoolAndHighConcurrency/src/main/java/com/cjs/ThreadLocal/ThreadLocalPractice.java
@@ -1,11 +1,13 @@
 package com.cjs.ThreadLocal;
 
 /**
- * 	线程局部变量，每个线程都为该线程局部变量保存相对应的值-具体保存在{@linkplain ThreadLocal}的内部类ThreadLocalMap里的Entry[]数组里面,
- * 数组的索引由对应的ThreadLocal对象里的threadLocalHashCode映射所得到（具体生成策略见源码），所以不会发生冲突（具体结合有道云笔记来学习）
- * 
- * ********Benefit:将变量的作用域变为线程所有，不是多个线程共享。
- * 
+ * 	线程局部变量，每个线程都为该线程局部变量保存相对应的值. 将变量的作用域变为线程内所有，不是多个线程共享.
+ *  使用场景: 全局传参, 比如分页参数的传递, Struts2的ActionContext的设置.
+ *
+ * 	实现方式: Thread里面有一个ThreadLocalMap的引用, 每一个ThreadLocal变量对应的
+ * 	值就会放在这个ThreadLocalMap({@linkplain ThreadLocal#ThreadLocalMap}里. 多线程运行下每个线程操作自己的TLM.
+ *  对应图片: https://github.com/KobeAndLebron/YoudaoNoteFileStorage/blob/master/concurrent/thread_local.png
+ *
  * 原理:
  * ThreadLocal里面get方法：
  * 	1、先获取当前线程-Thread.currentThread();
@@ -13,44 +15,49 @@ package com.cjs.ThreadLocal;
  * 		如果map为空(则创建map)或者此ThreadLocal对象对应的Entry对象为null，就调用initialValue方法（这个方法使用的是模版方法模式，
  * 可以由子类重写改变其行为，默认返回null），然后把这个初始值放入到ThreadLocalMap的Entry数组中;
  * 	ThreadLocal values pertaining to this thread. This map is maintained
- * 		by the ThreadLocal class. 
+ * 		by the ThreadLocal class.
  *  ThreadLocal.ThreadLocalMap threadLocals = null;
- *  3、根据当前的线程局部变量(ThreadLocal的threadLocalHashCode)和Entry数组长度(index = key.threadLocalHashCode & 
+ *  3、根据当前的线程局部变量(ThreadLocal的threadLocalHashCode)和Entry数组长度(index = key.threadLocalHashCode &
  *  	(table.length - 1))来获取此线程局部变量在当前线程的ThreadLocal.ThreadLocalGroup.Entry[]数组的索引
- *  
- *  set相同
- *  特殊的是第二步-不会调用initialValue方法，如果map为空的话，则根据当前线程和set的值还有当前ThreadLocal来设置/创建map
- * 
- * 	Struts2的ActionContext就是用的这种方法，此类的Integer相当于ActionContext，
- * 用这种方法就可以达到每个线程都有自己独立的ActionContext
- *  
+ *
+ * 	缺点:
+ * 	    使用不当容易造成内存泄漏, 在线程池的运行环境下, 假设有100个常驻线程, 现在有一个ThreadLocal变量, 那么每个线程都会有一个ThreadLocalMap
+ * 	    的来存这个TL变量, 100线程如果运行完, 由于这些线程是常驻线程, 所以一直不会被垃圾回收, 但是程序已经不会再使用它,  此时就造成了内存泄漏的情况.
+ *
+ * 	    ThreadLocalMap中Entry持有对ThreadLocal变量的弱引用, 目的: 减少内存泄漏的可能性.
+ * 	    当把ThreadLocal变量置为null的时候, 如果是弱引用的话, GC就会把这个ThreadLocal变量回收掉, 如果使用强引用
+ * 	    的话, 会导致这个ThreadLocal变量一直不会被回收, 造成内存泄漏的问题.
+ *
+ * 	正确做法: 使用完ThreadLocal变量后及时Remove.
+ *
  * @author 陈景帅
  *
  */
 public class ThreadLocalPractice {
-	final static int initialInteger = 1;
+	private static final int initialInteger = 1;
 
 	private static final ThreadLocal<Integer> integerThreadLocal = new ThreadLocal<Integer>(){
 		@Override
 		protected Integer initialValue() {
+		    integerThreadLocal.remove();
 			return initialInteger;
 		}
 	};
-	
+
 	public static void main(String[] args) {
-		System.out.println(ThreadLocalPractice.getContext());
-		ThreadLocalPractice.setContext(new Integer(3));
+		System.out.println(ThreadLocalPractice.getContext().toString());
+		ThreadLocalPractice.setContext(3);
 		Thread1 t1 = new Thread1();
 		Thread2 t2 = new Thread2();
 		t1.start();
 		t2.start();
-		System.out.println(ThreadLocalPractice.getContext());
+		System.out.println(ThreadLocalPractice.getContext().toString());
 	}
-	
+
 	public static void setContext(Integer value){
 		integerThreadLocal.set(value);
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T> T getContext(){
 		return (T) integerThreadLocal.get();
@@ -59,18 +66,18 @@ public class ThreadLocalPractice {
 
 class Thread1 extends Thread{
 	public void run(){
-		ThreadLocalPractice.setContext(new Integer(5));
+		ThreadLocalPractice.setContext(5);
 		/**
 		 * 去掉上面这句话,调用get方法就会调用initialValue方法
 		 */
-		System.out.println(ThreadLocalPractice.getContext());
+		System.out.println(ThreadLocalPractice.getContext().toString());
 	}
 }
 
 
 class Thread2 extends Thread{
 	public void run(){
-		ThreadLocalPractice.setContext(new Integer(4));
-		System.out.println(ThreadLocalPractice.getContext());
+		ThreadLocalPractice.setContext(4);
+		System.out.println(ThreadLocalPractice.getContext().toString());
 	}
 }
